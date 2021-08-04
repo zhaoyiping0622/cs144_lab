2f358076f0859431dac90e37cee7163af1018a28
zhaoyiping <zhaoyiping0622@gmail.com>
2021-08-05 00:36:27 +0800

    finish lab2 code formatted tests passed

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,5 +1,7 @@
 #include "tcp_receiver.hh"
 
+#include <iostream>
+
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -10,8 +12,29 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-void TCPReceiver::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPReceiver::segment_received(const TCPSegment &seg) {
+    WrappingInt32 seqno = seg.header().seqno;
+    if (!isn) {
+        if (!seg.header().syn)
+            return;
+        isn.emplace(seqno);
+        seqno = WrappingInt32(seqno.raw_value() + 1);
+    }
+    uint64_t index = unwrap(seqno, isn.value(), _reassembler.stream_out().bytes_written()) - 1;
+    bool eof = seg.header().fin;
+    _reassembler.push_substring(seg.payload().copy(), index, eof);
+}
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    optional<WrappingInt32> ret;
+    const ByteStream &stream = _reassembler.stream_out();
+    if (isn) {
+        if (stream.input_ended())
+            ret.emplace(wrap(stream.bytes_written() + 2, isn.value()));
+        else
+            ret.emplace(wrap(stream.bytes_written() + 1, isn.value()));
+    }
+    return ret;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
